e04661281f102ef76539f39c414853a6b28e2f54
Harpreet Singh <hsingh9600@gmail.com>
2020-09-06 22:00:56 +1200

    Added Deployment code

--- app.js
+++ app.js
@@ -216,6 +216,11 @@ function verifyToken(req, res, next) {
     }
 }
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
+    console.log("Server has started successfully");
 });
